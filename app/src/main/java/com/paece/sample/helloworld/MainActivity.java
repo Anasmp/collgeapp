2ce4aa134cec263f864dfc06d1a085513e0d4654
youngtechydotcom <youngtechy.com@gmail.com>
2018-01-14 23:07:24 +0530

    intial commit2

---
+++ app/src/main/java/com/paece/sample/helloworld/MainActivity.java
@@ -0,0 +1,69 @@
+package com.paece.sample.helloworld;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CardView cv1 = (CardView) findViewById(R.id.card_view);
+        CardView cv2 = (CardView) findViewById(R.id.card_view2);
+        final CardView cv3 = (CardView) findViewById(R.id.card_view3);
+        CardView cv4 = (CardView) findViewById(R.id.card_view4);
+
+
+        cv1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        cv2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), events.class);
+                startActivity(intent);
+            }
+        });
+
+        cv3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dialContactPhone("123123123");
+
+            }
+        });
+
+        cv4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                startActivity(browserIntent);
+
+            }
+        });
+
+
+
+
+
+    }
+
+    private void dialContactPhone(final String phoneNumber) {
+
+        startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null)));
+    }
+
+
+}
---
+++ app/src/main/java/com/paece/sample/helloworld/events.java
@@ -0,0 +1,13 @@
+package com.paece.sample.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class events extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events);
+    }
+}
